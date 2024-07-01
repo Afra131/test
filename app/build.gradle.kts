65a35301e5c1c3b2e6e0af56e776e17bbb4c787c
Afra131 <136213468+Afra131@users.noreply.github.com>
2024-06-30 20:45:17 -0700

    environment changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:$room_version")
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("androidx.core:core:1.7.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
